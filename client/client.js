d94084451303485fa955d5bbe4693294fb2f640c
Corey Loman <loman@seas.upenn.edu>
2014-01-07 20:13:53 -0500

    initial commit

---
+++ client/client.js
@@ -0,0 +1,52 @@
+Template.tweets.tweets = function() {
+	Tweets = Meteor.subscribe('tweets');
+}
+
+
+Template.new_tweet.events = {
+	'click #submit' : function (event) {
+			if (Meteor.user()) {
+				var author = Meteor.user().profile.name;
+				var body = document.getElementById('tweet');
+				if (body.value.length < 140 && body.value.length > -1) {
+					Tweets.insert({
+						author: author,
+						tweet: body.value,
+						time: Date.now(),
+					});
+					Template.new_tweet.tweet.value = ''; //maybe document.getEl?
+					chars = 140;
+				} else {
+					alert("Tweets cannot exceed 140 characters!");
+				}
+			} else {
+				alert("You must be logged in to post.");
+			}
+		},
+		
+
+	'keydown #tweet' : function(event) {
+		var body = document.getElementById('tweet');
+		if (body.value == "" || body.value == null) {
+			var remaining = 140;
+		} else {
+			var remaining = 141 - body.value.length;
+		}
+		console.log(remaining);
+		Template.new_tweet.chars = remaining;
+	}
+	
+}
+
+/*Template.new_tweet.chars.events = {
+	'keydown input#tweet' : function(event) {
+		var body = document.getElementById('tweet');
+		if (body == "" || body == null) return 140;
+		var remaining = 140 - body.length;
+		return remaining;
+	}
+}*/
+
+Template.tweets.logged_in = function() {
+	return (Meteor.user() != null)
+}
---
+++ server/models.js
@@ -0,0 +1,15 @@
+Tweets = new Meteor.Collection('tweets');
+
+Meteor.publish('tweets', function() {
+	// possibly overload this function to return the correct tweets
+	// for profiles
+	return Tweets.find({}, {sort: {time: -1, limit: 50}});
+});
+
+if (true) 
+	{Tweets.allow({
+		insert: function() {
+			return true;
+		}
+	});
+}
